810030d310c1a1ef2a6222172f9edaf81b5edb60
bhavleenxsingh <bhavleenxsingh@gmail.com>
2025-03-09 14:40:24 +0530

    Logic built.

--- app/src/main/java/com/example/weatherj/MainActivity.java
+++ app/src/main/java/com/example/weatherj/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.weatherj;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -43,9 +52,46 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String city = searchbox.getText().toString();
                 url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d73050f0c136744314578782b3409ede";
-
+                new Fetch().execute();
             }
         });
+    }
+        class Fetch extends AsyncTask<Void, Void, String>{
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL urlin = new URL(url);
+                    HttpsURLConnection conn = (HttpsURLConnection) urlin.openConnection();
+                    conn.setRequestMethod("GET");
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    StringBuilder resultw = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        resultw.append(line);
+                    }
+                    reader.close();
+                    return resultw.toString();
+                }
+                catch (Exception e){
+                 Toast.makeText(MainActivity.this, "Error! Please Try Again with Correct City!", Toast.LENGTH_SHORT).show();
+                return "Error : " + e.getMessage();
+                }
+            }
+
+            @Override
+            public void onPostExecute(String results){
+                try{
+                    JSONObject json = new JSONObject(results);
+                    JSONObject main  = json.getJSONObject("main");
+                    double temp = main.getDouble("temp");
+                    result.setText("Temperature is : " + temp + " C");
+                }
+                catch (Exception e){
+                    result.setText("Error loading Weather");
+                }
+            }
+
+        }
 
     }
-}
